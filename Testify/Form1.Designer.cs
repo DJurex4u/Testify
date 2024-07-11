a07218041406c1a6452292525439ebec805c8f66
Ivan Budoš <ibudos@etfos.hr>
2024-07-11 13:13:06 +0200

    quicksave

--- Testify/Form1.Designer.cs
+++ Testify/Form1.Designer.cs
@@ -28,35 +28,182 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.txtBaseUrl = new System.Windows.Forms.TextBox();
+            this.lblTestName = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbxHttpMethods = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblFirstVerification = new System.Windows.Forms.Label();
+            this.txtResponseCode = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // button1
+            // txtBaseUrl
             // 
-            this.button1.Location = new System.Drawing.Point(110, 371);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.txtBaseUrl.Location = new System.Drawing.Point(158, 29);
+            this.txtBaseUrl.Name = "txtBaseUrl";
+            this.txtBaseUrl.Size = new System.Drawing.Size(100, 20);
+            this.txtBaseUrl.TabIndex = 0;
+            // 
+            // lblTestName
+            // 
+            this.lblTestName.AutoSize = true;
+            this.lblTestName.Location = new System.Drawing.Point(13, 13);
+            this.lblTestName.Name = "lblTestName";
+            this.lblTestName.Size = new System.Drawing.Size(35, 13);
+            this.lblTestName.TabIndex = 1;
+            this.lblTestName.Text = "label1";
+            this.lblTestName.Click += new System.EventHandler(this.lblTestName_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "When I send";
+            // 
+            // cbxHttpMethods
+            // 
+            this.cbxHttpMethods.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbxHttpMethods.FormattingEnabled = true;
+            this.cbxHttpMethods.Items.AddRange(new object[] {
+            "GET",
+            "POST",
+            "PUT",
+            "DELETE"});
+            this.cbxHttpMethods.Location = new System.Drawing.Point(12, 95);
+            this.cbxHttpMethods.Name = "cbxHttpMethods";
+            this.cbxHttpMethods.Size = new System.Drawing.Size(69, 21);
+            this.cbxHttpMethods.TabIndex = 3;
+            this.cbxHttpMethods.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(87, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "request to";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(147, 95);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(248, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(401, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "endpoint";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 134);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "with headers";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(28, 161);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(394, 125);
+            this.textBox2.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(26, 325);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(395, 163);
+            this.textBox3.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(14, 298);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(51, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "and body";
+            // 
+            // lblFirstVerification
+            // 
+            this.lblFirstVerification.AutoSize = true;
+            this.lblFirstVerification.Location = new System.Drawing.Point(26, 516);
+            this.lblFirstVerification.Name = "lblFirstVerification";
+            this.lblFirstVerification.Size = new System.Drawing.Size(121, 13);
+            this.lblFirstVerification.TabIndex = 12;
+            this.lblFirstVerification.Text = "I verify response code is";
+            this.lblFirstVerification.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // txtResponseCode
+            // 
+            this.txtResponseCode.Location = new System.Drawing.Point(169, 513);
+            this.txtResponseCode.Name = "txtResponseCode";
+            this.txtResponseCode.Size = new System.Drawing.Size(100, 20);
+            this.txtResponseCode.TabIndex = 13;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(485, 675);
+            this.Controls.Add(this.txtResponseCode);
+            this.Controls.Add(this.lblFirstVerification);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cbxHttpMethods);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblTestName);
+            this.Controls.Add(this.txtBaseUrl);
             this.Name = "Form1";
             this.Text = "Testify";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtBaseUrl;
+        private System.Windows.Forms.Label lblTestName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbxHttpMethods;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblFirstVerification;
+        private System.Windows.Forms.TextBox txtResponseCode;
     }
 }
 
--- Testify/Form1.cs
+++ Testify/Form1.cs
@@ -26,5 +26,30 @@ namespace Testify
         {
 
         }
+
+        private void lblTestName_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
