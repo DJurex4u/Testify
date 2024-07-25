80916279d37da4e38129eeade4c7212fa78cc399
Ivan Budoš <ibudos@etfos.hr>
2024-07-25 13:42:46 +0200

    first request

--- Testify/Form1.cs
+++ Testify/Form1.cs
@@ -4,10 +4,14 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http.Headers;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml.Linq;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Text.Json;
 
 namespace Testify
 {
@@ -58,7 +62,7 @@ namespace Testify
 
         }
 
-        private void button1_Click_1(object sender, EventArgs e)
+        private async void button1_Click_1(object sender, EventArgs e)
         {
             StringBuilder text = new StringBuilder();
             text
@@ -71,13 +75,40 @@ namespace Testify
             .Append(" ")
             .Append(" kraj ");
             txtBody.Text = text.ToString();
-         
+
             if (comboHttpMethods.Text == "GET")
             {
                 txtBody.Text = "ssdfsdfsdfsdfsdfsdfsdffsfds";
             }
             //if (!string.IsNullOrWhiteSpace(txtName.Text) && !lstNames.Items.Contains(txtName.Text))
             //    lstNames.Items.Add(txtName.Text);
+
+            HttpClient client = GetHttpClient();
+            //HttpResponseMessage response = HttpClientExtensions.SendAsJsonAsync<Employee>(client, HttpMethod.Post, "emp/v1/emp/details/new", emp).Result;
+            //HttpContent content = new HttpContent();
+
+            StringContent content = new StringContent(JsonSerializer.Serialize(new
+            {
+                email = "client@example.com",
+                password = "123456"
+            }), Encoding.UTF8, "application/json");
+
+            var result = await client.PostAsync("client/", content);
+            string resultContent = await result.Content.ReadAsStringAsync();
+            Console.WriteLine(resultContent);
+        }
+
+        public static HttpClient GetHttpClient()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://127.0.0.1:8000/");
+            client.Timeout = new TimeSpan(0, 2, 0);
+            //client.DefaultRequestHeaders.Add("Authorization", SettingsBindableAttribute.Value.AccessToken);
+            //client.DefaultRequestHeaders.Add("Authorization", SettingsBindableAttribute.Value.AccessToken);
+
+            //var defaultRequestHeaders = client.DefaultRequestHeaders;
+            //client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            return client;
         }
     }
 }
